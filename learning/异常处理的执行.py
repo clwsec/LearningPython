8ab825e2d9d77a690626704bf3bb5866ffdd108a
clwsec <clwsec@163.com>
2019-05-18 20:20:18 +0800

    add some tests

--- learning/异常处理的执行.py
+++ learning/异常处理的执行.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # time: 2019/4/21
-a = 1
+# a = 1
+# try:
+#     a += 1
+# except:
+#     a += 1
+# else:  # 这里也会执行
+#     a += 1
+# finally:
+#     a += 1
+# print(a)
+
+
 try:
-    a += 1
-except:
-    a += 1
-else:  # 这里也会执行
-    a += 1
+    print('try...')
+    r = 10 / int('2')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+else:
+    print('no error!')
 finally:
-    a += 1
-print(a)
+    print('finally...')
+print('END')
--- learning/生成器.py
+++ learning/生成器.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# time:2019/5/2
+# time: 2019/5/2
 
 # 可写为生成器的函数中使用yield即为生成器
 def fib(max):
@@ -10,6 +10,7 @@ def fib(max):
         a, b = b, a + b
         n = n + 1
     return 'done'
+
 
 # generator函数的“调用”实际返回一个generator对象
 print(fib(6))
@@ -30,13 +31,14 @@ while True:
 
 
 # 杨辉三角生成器实现
-def triangles(n): # n 为需要生成的行数加1
+def triangles(n):  # n 为需要生成的行数加1
     p = [1]
     while True:
-        yield  p # generator函数与普通函数的差别：在执行过程中，遇到yield就中断，下次又继续执行
-        p = [1] + [p[i] + p[i+1] for i in range(len(p)-1)] + [1]
+        yield p  # generator函数与普通函数的差别：在执行过程中，遇到yield就中断，下次又继续执行
+        p = [1] + [p[i] + p[i + 1] for i in range(len(p) - 1)] + [1]
         if len(p) == n:
             break
+
 
 n = 11
 for t in triangles(n):
